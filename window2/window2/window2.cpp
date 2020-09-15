2eb1c6cf6b0b9afcec54b6600c3be7bcb5733121
ASUS <Nairaceorex>
2020-09-15 15:10:56 +0300

    open

--- window2/window2/window2.cpp
+++ window2/window2/window2.cpp
@@ -1,76 +1,56 @@
 ﻿#include <windows.h>
 #include <tchar.h>
+
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
 TCHAR WinName[] = _T("MainFrame");
-int APIENTRY _tWinMain(HINSTANCE This, // WeckpuntTop TeKyulero NpusIoKXeHUAa
-	HINSTANCE Prev, // B COBpeMeHHEIX CUcTeMax BCerga 0
-	LPTSTR cmd, // KoMaHgHaa crpoKa
-	int mode) // PexyvM oTO6paxeHua OKHAa
+int APIENTRY _tWinMain(HINSTANCE This, // Дескрипотор текущего приложения
+	HINSTANCE Prev, // В современных системах всегда 0
+	LPTSTR cmd, // Командная строка
+	int mode) // Режим отображения окна
 {
-	HWND hWnd; // Weckpuntop miaBHOTO OKHa MporpaMmmel
-	MSG msg; // CTpykTypa JIA xpaHeHuaA COOobleHNA
-
-	WNDCLASS wc; // Kmnacc oKHa
-	// OmpeneneHue Kiacca oKHa
-
-	wc.hInstance = This;
-
-	wc.lpszClassName = WinName; // Vima Klacca OKHa
-	wc.lpfnWndProc = WndProc; // SYHKUNA OKHa
-	wc.style = CS_HREDRAW | CS_VREDRAW; // CTUIIb OKHa
-
-	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // CTaHDaptHad UKOHKa
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // CTaHaptTHpm Kypcop
-	wc.lpszMenuName = NULL; // Het meHto
-
-	wc.cbClsExtra = 0; // Hert DONONHVTeEIbHBIX DAHHbIX KyIaCCca
-	wc.cbWndExtra = 0; // Het DONONHMTeIbHBIX TaHHbIX OKHAa
-
-	// 3anosHeHVe OKHa 6esIbIM LIBeETOM
-
+	HWND hWnd; // Дескриптор главного окна программы
+	MSG msg; // Структура для хранения сообщения
+	WNDCLASS wc; // Класс окна
+	wc.hInstance = This; // Определение класса окна
+	wc.lpszClassName = WinName; // Имя класса окна
+	wc.lpfnWndProc = WndProc; // Функция окна
+	wc.style = CS_HREDRAW | CS_VREDRAW; // Стиль окна
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION); // Стандартная икона
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Стандартный курсор
+	wc.lpszMenuName = NULL; // Нет меню
+	wc.cbClsExtra = 0; // Нет дополнительных данных класса
+	wc.cbWndExtra = 0; // Нет дополнительных данных окна
 	wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-
-	if (!RegisterClass(&wc)) return 0; // PeructpaluMa Kiacca OKHa
-	// CosqaHue oKHa
-
-	hWnd = CreateWindow (WinName, _T("KapKac Windows-npunoxeHua"), 
-
-		WS_OVERLAPPEDWINDOW, // CTVJIb OKHA
-		CW_USEDEFAULT, // x
-		CW_USEDEFAULT, // y —- PasmepbI OKHa
+	if (!RegisterClass(&wc)) return 0; // Регистрация класса окна
+	// Создание окна
+	hWnd = CreateWindow(WinName, // Имя класса окна
+		_T("Каркас приложения"), // Заголовок окна
+		WS_OVERLAPPEDWINDOW, // Стиль окна
+		CW_USEDEFAULT, // х
+		CW_USEDEFAULT, // у
 		CW_USEDEFAULT, // Width
 		CW_USEDEFAULT, // Height
-		HWND_DESKTOP, // Jleckpuntop ponutTenbcKorTo oKHa
-
-		NULL, // Het meuro
-		This, // Teckpuntop npunoxennua
-		NULL); // JononHYTeNnbHOU unbopmauyu HeT
-
-	ShowWindow(hWnd, mode); //Hoxa3atTb OKHO
-	// UKs o6pa6otKu coobuexun
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
-		TranslateMessage(&msg);// ®yHKUMA TpaHCNAWMM KONOB HaxaTON KnaBMUN
-		DispatchMessage(&msg); // Tocbmaer coobmenue oyHKuMM WndProc ()
-
+		HWND_DESKTOP, // Дескрипт приложения
+		NULL, // Нет меню
+		This, // Дескриптор приложения
+		NULL); // Дополнительной информации нет
+	ShowWindow(hWnd, mode); // Показать окно
+	// Цикл обработки сообщений
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg); // Функция трансляции кодов нажатой клавиши
+		DispatchMessage(&msg); // Посылает сообщения функции WndProc
 	}
-
 	return 0;
 }
-// OKOHHaA OYHKUMA BEISIBAeCTCA OMepalMOHHOM cucTeMoi
-// w“ nosmyuaeT coobileHua “3 OUepenU TIA DaHHOro mpunoxeHua
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message,
-	WPARAM wParam, LPARAM lParam)
-{ // O6pa6oTuuk coobueHun
-	switch (message)
+// Оконная функция вызывается ОС
+// и получает сообщение из очереди для данного приложения
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messege,
+	WPARAM wParam, LPARAM lParam) { // Обработчик сообщений
+	switch (messege)
 	{
 	case WM_DESTROY: PostQuitMessage(0);
-		break; // 3aBepuieHvue nporpammsl
-		// O6pa6orKa coobmeHuA nO ymMonuaHMo
-	default: return DefWindowProc(hWnd, message, wParam, lParam);
-
+		break; //Завершении программы
+	default: return DefWindowProc(hWnd, messege, wParam, lParam);
 	}
-
 	return 0;
 }
