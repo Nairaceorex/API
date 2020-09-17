01732671dfb31b64c83046dfbcb69f562c32cd0f
ASUS <Nairaceorex>
2020-09-17 20:33:05 +0300

    add files

--- window3/WindowsProject1/WindowsProject1.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include <windows.h>
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
-	MessageBox(NULL, L"Hello, Win32 world!", L"Hello from Message Box", MB_OK);
-	return 0;
-}
