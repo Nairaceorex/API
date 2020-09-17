81dce7ee8321588f50f7a35ada1dbedf251862a9
Nairaceorex <63120787+Nairaceorex@users.noreply.github.com>
2020-09-17 14:20:22 +0300

    Add files via upload

---
+++ window3/WindowsProject1/WindowsProject1.cpp
@@ -0,0 +1,6 @@
+﻿#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	MessageBox(NULL, L"Hello, Win32 world!", L"Hello from Message Box", MB_OK);
+	return 0;
+}
